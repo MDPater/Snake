ea974c3f72280450adbb355e7611077b4223ec3c
MDPater <depatermax@gmail.com>
2023-07-07 00:06:58 +0200

    draw Snake and Food

--- snake.js
+++ snake.js
@@ -5,16 +5,36 @@ var columns = 20;
 var board;
 var context;
 
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//food
+var foodX;
+var foodY;
+
 window.onload = function(){
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = columns * blockSize;
     context = board.getContext("2d"); 
 
+    placeFood();
     update();
 }
 
 function update(){
     context.fillStyle="black";
     context.fillRect(0,0, board.width, board.height);
+
+    context.fillStyle="lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle="Red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function placeFood(){
+    foodX = Math.floor(Math.random() * columns) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
 }
